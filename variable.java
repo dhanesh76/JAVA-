93472bd60e5d633a2d01a5f20e1037b3377d4ef4
dhanesh <134997867+dhanesh76@users.noreply.github.com>
2024-07-05 11:30:02 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,21 @@
+//Variable is a container which behave as a value that it contains 
+
+public class Main {
+    public static void main(String[] args) {
+        // Primitive data types
+        int age = 30;
+        double height = 5.9;
+        char gender = 'M';
+        boolean isStudent = true;
+        
+        // Output
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Gender: " + gender);
+        System.out.println("Is a student? " + isStudent);
+        
+        // Reference data types
+        String name = "John Doe";
+        System.out.println("Name: " + name);
+    }
+}
